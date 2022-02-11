46851b0d82846f67d2870e57a132c2b67f62b536
Braien Machado de Paula <braienmp@outlook.com>
2022-02-11 11:24:50 -0300

    create tests for navbar in header

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+describe('test header', () => {
+  render(<App />);
+
+  it('has a navbar', () => {
+    const navElement = screen.getByRole('nav');
+    expect(navElement).toBeInTheDocument();
+  });
+  it('has all the links', () => {
+    const navLinks = screen.getByRole('nav').innerHTML;
+
+    expect(navLinks).toHaveLength(4);
+
+    const linksText = ['Quem sou', 'Habilidades', 'Projetos', 'Contato'];
+
+    navLinks.forEach((link, index) => {
+      expect(link).toHaveTextContent(linksText[index]);
+    });
+  })
+})
+
 describe('test presentation', () => {
   it('renders the right content', () => {
     render(<App />);
