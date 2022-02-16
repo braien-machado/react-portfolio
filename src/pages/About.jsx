9b53318682814eba41c7bc6834b4ce61d4a4f80c
Braien Machado de Paula <braienmp@outlook.com>
2022-02-16 18:44:45 -0300

    set initial style to h1 in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 export default function About() {
   return (
     <main className='h-screen py-12 bg-gray-900 text-white'>
-      <h1 className='flex flex-col text-center'>Olá! Eu sou
-        <span>Braien Machado.</span>
-        <span>Desenvolvedor Front-end</span></h1>
+      <h1 className='flex flex-col text-center items-center m-auto pt-2'>Olá! Meu nome é
+        <span className='text-5xl border-b-2 w-fit'>Braien Machado.</span>
+        <span className='pt-2'>Desenvolvedor Front-end</span>
+      </h1>
+
     </main>
   )
 }
