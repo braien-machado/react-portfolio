3dcdb066d231cfdc8c18d53d1cf29e712d006a1d
Braien Machado de Paula <braienmp@outlook.com>
2022-02-16 15:39:35 -0300

    finish header's mobile style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,29 +3,34 @@ import { GiBattleAxe, GiHamburgerMenu } from 'react-icons/gi';
 import { GrClose } from 'react-icons/gr';
 
 export default function Header () {
-  const [isHidden, setIsHidden] = useState('hidden');
+  const [isMenuHidden, setIsMenuHidden] = useState(true);
 
   const toggleMenu = () => {
-    setIsHidden(isHidden === 'hidden' ? '' : 'hidden')
+    setIsMenuHidden(isMenuHidden ? false : true);
   };
 
+  const menuLinks = ['Sobre mim', 'Habilidades', 'Projetos', 'Contato'];
+
+  const generateMenuLink = (link) => {
+    return (
+      <li className='mt-4 hover:underline hover:font-medium hover:cursor-pointer text-white'>{ link }</li>
+    );
+  }
+
   return (
-    <header className='flex justify-between p-2'>
-      <GiBattleAxe size={30} />
-      <nav className={`${isHidden}`}>
-        <ul className='flex justify-around'>
-          <li>Sobre mim</li>
-          <li>Habilidades</li>
-          <li>Projetos</li>
-          <li>Contato</li>
+    <header className='flex justify-between p-2 relative bg-gray-900'>
+      <GiBattleAxe className='text-white' size={30} />
+      <nav className={`${isMenuHidden ? '-left-60' : 'left-0'} absolute top-0 w-60 bg-gray-900 h-screen duration-300`}>
+        <ul className='flex flex-col items-center text-lg w-fit m-auto'>
+          { menuLinks.map((link) => generateMenuLink(link)) }
         </ul>
       </nav>
       <button className='w-fit'>
         {
-          isHidden ? (
-            <GiHamburgerMenu size={30} onClick={ toggleMenu }/>
+          isMenuHidden ? (
+            <GiHamburgerMenu className='text-white' size={30} onClick={ toggleMenu }/>
           ) : (
-            <GrClose size={30} onClick={ toggleMenu }/>
+            <GrClose className='bg-white' size={30} onClick={ toggleMenu }/>
           )
         }
       </button>
