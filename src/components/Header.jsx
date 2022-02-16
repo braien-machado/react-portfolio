fad3942ac188cdbb2bc482262f79d406608ea228
Braien Machado de Paula <braienmp@outlook.com>
2022-02-16 14:37:59 -0300

    add icons to header and start mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { GiBattleAxe, GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 
-export default function Header() {
+export default function Header () {
+  const [isHidden, setIsHidden] = useState('hidden');
+
+  const toggleMenu = () => {
+    setIsHidden(isHidden === 'hidden' ? '' : 'hidden')
+  };
+
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>Quem Sou</li>
+    <header className='flex justify-between p-2'>
+      <GiBattleAxe size={30} />
+      <nav className={`${isHidden}`}>
+        <ul className='flex justify-around'>
+          <li>Sobre mim</li>
           <li>Habilidades</li>
           <li>Projetos</li>
           <li>Contato</li>
         </ul>
       </nav>
+      <button className='w-fit'>
+        <GiHamburgerMenu size={30} onClick={ toggleMenu }/>
+      </button>
     </header>
   )
 }
