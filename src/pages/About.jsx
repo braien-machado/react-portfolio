0345ed62eefd1a04f63a2a07ab3198fe00c2fdf0
Braien Machado de Paula <braienmp@outlook.com>
2022-02-16 16:46:42 -0300

    create basic About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <main>
+      <h1>Olá! Eu sou
+        <span>Braien Machado.</span>
+        <span>Desenvolvedor Front-end</span></h1>
+    </main>
+  )
+}
