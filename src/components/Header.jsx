b2fd596e4ff06490ae8198530abc5891ce25569a
Braien Machado de Paula <braienmp@outlook.com>
2022-02-16 13:30:19 -0300

    create components folder and basic Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Quem Sou</li>
+          <li>Habilidades</li>
+          <li>Projetos</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
