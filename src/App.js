622c5374d1da39760c793b703eae017bd188c857
Braien Machado de Paula <braienmp@outlook.com>
2022-02-16 13:48:21 -0300

    remove header content from App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <nav>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <section id="presentation">
+          <h1>Olá! Eu sou <span>Braien Machado.</span> <span>Desenvolvedor Front-end</span></h1>
+        </section>
+        <section>
           <ul>
-            <li>Quem Sou</li>
-            <li>Habilidades</li>
-            <li>Projetos</li>
-            <li>Contato</li>
+            <h2>Hard Skills</h2>
+            <li data-testid="hard-skill">React</li>
           </ul>
-        </nav>
-      </header>
-      <section id="presentation">
-        <h1>Olá! Eu sou <span>Braien Machado.</span> <span>Desenvolvedor Front-end</span></h1>
-      </section>
-      <section>
-        <ul>
-          <h2>Hard Skills</h2>
-          <li data-testid="hard-skill">React</li>
-        </ul>
-        <ul>
-          <h2>Soft Skills</h2>
-          <li data-testid="soft-skill">Colaboração</li>
-        </ul>
-        <a href='#cv' id="cv">Veja meu currículo</a>
-      </section>
-      <section>
-        <h2 data-testid="projects-title">Projetos</h2>
-      </section>
-      <section>
-        <h2 data-testid="contact-title">Informações de Contato</h2>
-      </section>
-      <footer role="contentinfo">
-        <span data-testid="footer-text">desenvolvido por Braien Machado.</span>
-      </footer>
-    </div>
+          <ul>
+            <h2>Soft Skills</h2>
+            <li data-testid="soft-skill">Colaboração</li>
+          </ul>
+          <a href='#cv' id="cv">Veja meu currículo</a>
+        </section>
+        <section>
+          <h2 data-testid="projects-title">Projetos</h2>
+        </section>
+        <section>
+          <h2 data-testid="contact-title">Informações de Contato</h2>
+        </section>
+        <footer role="contentinfo">
+          <span data-testid="footer-text">desenvolvido por Braien Machado.</span>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
