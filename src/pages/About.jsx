dc2690c9933ca81ddb9caf82d31f75c3b54c714c
Braien Machado de Paula <braienmp@outlook.com>
2022-02-16 18:18:41 -0300

    change background of About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
   return (
-    <main className='h-screen py-12 bg-black text-white'>
+    <main className='h-screen py-12 bg-gray-900 text-white'>
       <h1 className='flex flex-col text-center'>Olá! Eu sou
         <span>Braien Machado.</span>
         <span>Desenvolvedor Front-end</span></h1>
