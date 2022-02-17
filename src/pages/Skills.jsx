2682750ff9d258031b7568f24bc6c17827e65456
Braien Machado de Paula <braienmp@outlook.com>
2022-02-17 11:21:22 -0300

    basic style for Skills page and import needed component

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import HardSkillsCards from '../components/HardSkillsCards';
+
 
 export default function Skills() {
   return (
-    <main>
-      <ul>
+    <main className='min-h-screen py-12  bg-gray-900 text-white'>
+      <h1>Hard Skills</h1>
+      <HardSkillsCards />
+      {/* <ul>
         <h2>Hard Skills</h2>
         <li data-testid="hard-skill">React</li>
       </ul>
@@ -11,7 +15,7 @@ export default function Skills() {
         <h2>Soft Skills</h2>
         <li data-testid="soft-skill">Colaboração</li>
       </ul>
-      <a href='#cv' id="cv">Veja meu currículo</a>
+      <a href='#cv' id="cv">Veja meu currículo</a> */}
     </main>
   )
 }
