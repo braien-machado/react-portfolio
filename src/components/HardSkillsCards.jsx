3e1b14d03c072e31c19df3204b6119e51cce421b
Braien Machado de Paula <braienmp@outlook.com>
2022-02-17 11:20:23 -0300

    create component for hard skills cards

---
+++ src/components/HardSkillsCards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaReact } from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io';
+import { AiFillHtml5 } from 'react-icons/ai';
+
+export default function HardSkills() {
+  return (
+    <div>
+      <AiFillHtml5 />
+      <IoLogoJavascript />
+      <FaReact />
+    </div>
+  )
+}
