851fb20ea880c3f0d12a2388f12062e0f3be3aec
Braien Machado de Paula <braienmp@outlook.com>
2022-02-17 09:49:51 -0300

    style NotFound page and set min-width to body

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <main>
-      <div>
-        <img className='w-20' src={require('../images/broken-axe.png')} alt="broken axe" />
-        <h1>Erro 404</h1>
+    <main className='py-12 h-screen flex flex-col justify-center'>
+      <div className='flex justify-center'>
+        <img className='object-scale-down h-28 w-28' src={require('../images/broken-axe.png')} alt="broken axe" />
+        <h1 className='text-6xl flex flex-col justify-center pl-2'>
+          <span>Erro</span>
+          <span>404</span>
+        </h1>
       </div>
-      <div>
-        <h2>Página não encontrada</h2>
+      <div className='text-center'>
+        <h2 className='font-medium text-lg'>Página não encontrada :(</h2>
         <p>Utilize o menu de navegação para ir para outra página.</p>
       </div>
     </main>
