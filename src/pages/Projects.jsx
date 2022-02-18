fe8cc47fa4743b99b228c530d11c7fdd7d5e1e68
Braien Machado de Paula <braienmp@outlook.com>
2022-02-18 11:31:28 -0300

    add basic style to Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProjectsCarousel from '../components/ProjectsCarousel';
 
 export default function Projects() {
   return (
-    <main>
-      <h1>Projetos</h1>
+    <main className='min-h-screen py-12  bg-gray-900 text-white'>
+      <h1 className='text-4xl text-center my-4'>Projetos</h1>
+      <ProjectsCarousel />
     </main>
-  )
+  );
 }
