950b6112e5ee3cd38040f669a8120e3aa82bac6c
Braien Machado de Paula <braienmp@outlook.com>
2022-03-01 10:39:01 -0300

    add key prop

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard(props) {
 
   if (!name) {
     return (
-      <div className='border-gray-800 border-2 hover:border-gray-300 shadow-md duration-300 rounded-md w-4/5 p-2 mb-4'>
+      <div key={index} className='border-gray-800 border-2 hover:border-gray-300 shadow-md duration-300 rounded-md w-4/5 p-2 mb-4'>
         <h1 className='text-2xl font-medium'>Em breve</h1>
         <p className='italic text-gray-500'>Projeto em desenvolvimento...</p>
       </div>
