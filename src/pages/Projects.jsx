48ecd2e4e14da8284d0c9a00db6b37377f0f72cf
Braien Machado de Paula <braienmp@outlook.com>
2022-03-01 10:38:06 -0300

    remove unneeded component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import ProjectsLibrary from '../components/ProjectsLibrary';
+import ProjectCard from '../components/ProjectCard';
+import projectsInfo from '../projectsInfo';
 
 export default function Projects() {
   return (
     <main className='min-h-screen py-12  bg-gray-900 text-white'>
       <h1 className='text-4xl text-center my-4'>Projetos</h1>
-      <ProjectsLibrary />
+      <div className='flex justify-evenly flex-wrap'>
+        { projectsInfo.map((project, index) => (
+          <ProjectCard project={project} index={index} key={index} />
+        )) }
+      </div>
     </main>
   );
 }
