58f8f6ace629888c5a382f3370b22e88db80112b
Braien Machado de Paula <braienmp@outlook.com>
2022-04-13 11:34:34 -0300

    change image for rest-countries-project

--- src/projectsInfo.js
+++ src/projectsInfo.js
@@ -14,7 +14,7 @@ const projectsInfo = [
   {
     name: 'Where in the world?',
     description: 'A aplicação disponibiliza cards com informações básicas de diversos países, com opções de filtragem por nome e/ou região. Além disso, há a possibilidade de alterar entre modo escuro e claro. O usuário pode clicar em um card para ser redirecionado para uma tela com mais informações, como a moeda local. Da página de detalhes, também é possível acessar mais detalhes de qualquer país que faz fronteira com o selecionado, além de poder voltar à página inicial.',
-    image: 'mobile-detail-light.png',
+    image: 'desktop-filter-dark.png',
     demo: 'https://countries-bmp.vercel.app/',
     repository: 'https://github.com/braien-machado/rest-countries-api-with-color-theme-switcher',
     stacks: [
