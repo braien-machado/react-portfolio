57ff55fef7770675d0c9cce86a95dcb3c56d7f25
Braien Machado de Paula <braienmp@outlook.com>
2022-04-13 10:56:31 -0300

    add rest-countries-api project info

--- src/projectsInfo.js
+++ src/projectsInfo.js
@@ -1,8 +1,7 @@
 const projectsInfo = [
-  {},
   {
     name: 'React Portfolio',
-    description: 'Portfolio feito em React',
+    description: 'Portfolio feito em React para exposição de projetos, informações de contato e um pouco sobre mim.',
     image: 'portfolio-react.png',
     demo: 'https://braien-machado.vercel.app/',
     repository: 'https://github.com/braien-machado/react-portfolio',
@@ -12,6 +11,20 @@ const projectsInfo = [
       'Html',
     ],
   },
+  {
+    name: 'Where in the world?',
+    description: 'A aplicação disponibiliza cards com informações básicas de diversos países, com opções de filtragem por nome e/ou região. Além disso, há a possibilidade de alterar entre modo escuro e claro. O usuário pode clicar em um card para ser redirecionado para uma tela com mais informações, como a moeda local. Da página de detalhes, também é possível acessar mais detalhes de qualquer país que faz fronteira com o selecionado, além de poder voltar à página inicial.',
+    image: 'mobile-detail-light.png',
+    demo: 'https://countries-bmp.vercel.app/',
+    repository: 'https://github.com/braien-machado/rest-countries-api-with-color-theme-switcher',
+    stacks: [
+      'React',
+      'Sass',
+      'Airbnb style',
+      'Rest API consumption'
+    ],
+  },
+  {},
 ];
 
 export default projectsInfo;
