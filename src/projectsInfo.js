4f9b18163c442600af1bbca201cefe00f38e5935
Braien Machado de Paula <braienmp@outlook.com>
2022-04-18 11:51:58 -0300

    Display newest project first

--- src/projectsInfo.js
+++ src/projectsInfo.js
@@ -1,17 +1,5 @@
 const projectsInfo = [
   {
-    name: 'React Portfolio',
-    description: 'Portfolio feito em React para exposição de projetos, informações de contato e um pouco sobre mim.',
-    image: 'portfolio-react.png',
-    demo: 'https://braien-machado.vercel.app/',
-    repository: 'https://github.com/braien-machado/react-portfolio',
-    stacks: [
-      'React',
-      'Tailwind Css',
-      'Html',
-    ],
-  },
-  {
     name: 'Where in the world?',
     description: 'A aplicação disponibiliza cards com informações básicas de diversos países, com opções de filtragem por nome e/ou região. Além disso, há a possibilidade de alterar entre modo escuro e claro. O usuário pode clicar em um card para ser redirecionado para uma tela com mais informações, como a moeda local. Da página de detalhes, também é possível acessar mais detalhes de qualquer país que faz fronteira com o selecionado, além de poder voltar à página inicial.',
     image: 'desktop-filter-dark.png',
@@ -22,6 +10,18 @@ const projectsInfo = [
       'Sass',
       'Airbnb style',
       'Rest API consumption'
+    ],
+  },
+  {
+    name: 'React Portfolio',
+    description: 'Portfolio feito em React para exposição de projetos, informações de contato e um pouco sobre mim.',
+    image: 'portfolio-react.png',
+    demo: 'https://braien-machado.vercel.app/',
+    repository: 'https://github.com/braien-machado/react-portfolio',
+    stacks: [
+      'React',
+      'Tailwind Css',
+      'Html',
     ],
   },
   {},
