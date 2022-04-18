f80b9c16e0093d319396c8efa4d174a2eefed077
Braien Machado de Paula <braienmp@outlook.com>
2022-04-18 11:33:41 -0300

    Change 'about' text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,13 +7,12 @@ export default function About() {
     <main className='min-h-screen py-12 bg-zinc-800 text-white flex flex-col items-center'>
       <h1 className='flex flex-col text-center items-center mt-4'>Olá! Meu nome é
         <span className='text-5xl'>Braien Machado.</span>
-        <span className='pt-2'>Desenvolvedor Front-end</span>
+        <span className='pt-2'>Desenvolvedor Full Stack</span>
       </h1>
       <img className='rounded-full w-36 shadow-md shadow-black mt-4' src={ profilePic } alt="Braien Machado" />
       <div className='px-4 text-justify max-w-screen-sm mt-4'>
         <p>Brasileiro, natural de Nova Friburgo, cidade da região serrana do estado do Rio de Janeiro. Sou casado e tenho 24 anos.</p>
-        <p className='mt-4'>Trabalho desde 2018 na área de Design Gráfico, Arte-finalização e produção de impressos. Ingressei no curso de Desenvolvedor FullStack da <a className='hover:underline' href='https://www.betrybe.com/' target='_blank' rel='noreferrer'>Trybe</a>, o qual tem previsão de encerramento em julho de 2022, com o objetivo de mudar meu campo de atuação e expandir meus horizontes no mercado de trabalho.
-        Em fevereiro de 2022 recebi meu certificado de Desenvolvedor Front-End e atualmente estou estudando Back-End.</p>
+        <p className='mt-4'>Trabalhei de 2018 a 2022 na área de Design Gráfico, Arte-finalização e produção de impressos. Em julho de 2021, decidi por mudar de carreira e iniciei uma formação de Desenvolvimento Web Full Stack na Trybe. Atualmente já tenho experiência com projetos pessoais e do próprio curso tanto na área de Front End quanto de Back End e estou aberto a propostas para  trabalhar em empresas que poderão me ajudar a desenvolver minhas habilidades e a aprender novas técnicas e ferramentas.</p>
       </div>
     </main>
   );
