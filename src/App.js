b9c4683fda8e44f0f4fc797abd4d2dd9d8595169
Braien Machado de Paula <braienmp@outlook.com>
2022-04-22 14:34:00 -0300

    remove routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './pages/About';
 import Skills from './pages/Skills';
 import Projects from './pages/Projects';
-import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Header />
-
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-
+      <About />
+      <Skills />
+      <Projects />
       <Footer />
-    </BrowserRouter>
+    </div>
   );
 }
 
