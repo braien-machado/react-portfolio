9b229ce35445c57b4ca7ca1bd21b84b0e133f6f3
Braien Machado de Paula <braienmp@outlook.com>
2022-04-22 14:34:56 -0300

    change link to new path to anchor by id

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
 import { GiBattleAxe, GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineGithub, AiFillLinkedin } from 'react-icons/ai';
 
 export default function Header () {
   const [isMenuHidden, setIsMenuHidden] = useState(true);
-  const { pathname } = useLocation();
+  const [section, setSection] = useState('#about');
 
   const toggleMenu = () => {
     setIsMenuHidden(isMenuHidden ? false : true);
   };
 
   const menuLinks = [
-    { link: 'Sobre mim', path: '/'},
-    { link: 'Habilidades', path: '/skills'},
-    { link: 'Projetos', path: '/projects'},
+    { link: 'Sobre mim', id: '#about'},
+    { link: 'Habilidades', id: '#skills'},
+    { link: 'Projetos', id: '#projects'},
   ];
 
-  const generateMenuLink = ({ link, path }, index) => {
-    if (pathname === path) {
+  const generateMenuLink = ({ link, id }) => {
+    if (section === id) {
       return (
         <li key={ link } className='py-4 w-60 text-center text-white bg-zinc-900'>
         { link }
@@ -26,16 +25,19 @@ export default function Header () {
       )
     }
     return (
-      <Link key={ link } to={ path } onClick={ toggleMenu } >
+      <a href={id} key={ link } to={ id } onClick={ () => {
+        setSection(id);
+        toggleMenu()
+      }}>
         <li className='py-4 w-60 text-center hover:bg-zinc-500 hover:cursor-pointer text-white duration-200'>
           { link }
         </li>
-      </Link>
+      </a>
     );
   };
 
   return (
-    <header className='flex justify-between p-2 fixed w-full bg-zinc-800 shadow-zinc-700 shadow-sm z-10'>
+    <header className='flex justify-between p-2 fixed w-full bg-zinc-800 z-10'>
       <GiBattleAxe className='text-white' size={30} />
       <nav className={`${isMenuHidden ? '-left-60' : 'left-0'} flex flex-col justify-between items-center absolute top-0 w-60 bg-zinc-800 h-screen duration-300 z-10`}>
         <ul className='flex flex-col items-center text-lg w-fit'>
@@ -54,7 +56,7 @@ export default function Header () {
         </div>
       </nav>
       { isMenuHidden ? null : (
-        <button className={`fixed bg-black opacity-80 w-screen h-screen top-0 left-0`} onClick={toggleMenu}></button>
+        <button className={`fixed cursor-default bg-black opacity-80 w-screen h-screen top-0 left-0`} onClick={toggleMenu}></button>
       )}
       <button className='w-fit'>
         <GiHamburgerMenu className='text-white' size={30} onClick={ toggleMenu }/>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const profilePic = require('../images/profile-pic.jpeg');
 // add typing animation do "Desenvolvedor Front-end"
 export default function About() {
   return (
-    <main className='min-h-screen py-12 bg-zinc-800 text-white flex flex-col items-center'>
+    <main id="about" className='min-h-screen py-12 bg-zinc-800 text-white flex flex-col items-center'>
       <h1 className='flex flex-col text-center items-center mt-4'>Olá! Meu nome é
         <span className='text-5xl'>Braien Machado.</span>
         <span className='pt-2'>Desenvolvedor Full Stack</span>
