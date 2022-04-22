744c0b8738b3321c90854d9597ada48bd2343c0b
Braien Machado de Paula <braienmp@outlook.com>
2022-04-22 17:06:46 -0300

    move github and linkedin links to home section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { GiBattleAxe } from 'react-icons/gi';
 import DesktopNavMenu from './DesktopNavMenu';
 import MobileNavMenu from './MobileNavMenu';
 
 export default function Header () {
   return (
-    <header className='flex justify-between p-2 fixed w-full bg-zinc-800 z-10'>
-      <GiBattleAxe className='text-white' size={30} />
+    <header className='flex justify-end p-2 fixed w-full bg-zinc-800 z-10'>
       <DesktopNavMenu />
       <MobileNavMenu />
     </header>
--- src/components/MobileNavMenu.jsx
+++ src/components/MobileNavMenu.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import { AiOutlineGithub, AiFillLinkedin } from 'react-icons/ai';
-import { GiHamburgerMenu, GiBattleAxe } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import MenuLinks from './MenuLinks';
 
-export default function MobileNavMenu(props) {
+export default function MobileNavMenu() {
   const [isMenuHidden, setIsMenuHidden] = useState(true);
 
   const toggleMenu = () => {
@@ -14,17 +13,6 @@ export default function MobileNavMenu(props) {
     <div className="sm:hidden h-7">
       <nav className={`${isMenuHidden ? '-left-60' : 'left-0'} flex flex-col justify-between items-center absolute top-0 w-60 bg-zinc-800 h-screen duration-300 z-10`}>
         <MenuLinks toggleMenu={toggleMenu}/>
-        <div className='flex w-full text-white justify-evenly'>
-          <a className='hover:text-blue-400 duration-200' href='https://github.com/braien-machado' target='_blank' rel='noreferrer'>
-              <AiOutlineGithub size={50} />
-          </a>
-          <a className='hover:text-blue-400 duration-200' href='https://linkedin.com/in/braien-machado' target='_blank' rel='noreferrer'>
-            <AiFillLinkedin size={50} />
-          </a>
-        </div>
-        <div className='pb-14'>
-          <GiBattleAxe className='text-white' size={100} />
-        </div>
       </nav>
       { isMenuHidden ? null : (
         <button className={`fixed cursor-default bg-black opacity-80 w-screen h-screen top-0 left-0`} onClick={toggleMenu}></button>
