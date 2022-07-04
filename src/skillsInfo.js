ee61cd3d3bee2110c607e2e34f2397951e2cd470
Braien Machado de Paula <braienmp@outlook.com>
2022-07-04 10:52:53 -0300

    feat: add new hard skills

--- src/skillsInfo.js
+++ src/skillsInfo.js
@@ -1,25 +1,50 @@
-import { FaReact, FaDocker, FaNodeJs, FaHandHoldingHeart } from 'react-icons/fa';
+import {
+  FaReact,
+  FaDocker,
+  FaNodeJs,
+  FaHandHoldingHeart
+} from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io';
 import { AiFillHtml5, AiOutlineGithub } from 'react-icons/ai';
-import { DiCss3 } from 'react-icons/di';
-import { SiJest, SiRedux, SiTestinglibrary } from 'react-icons/si';
+import {
+  DiCss3,
+  DiSass,
+  DiMongodb
+} from 'react-icons/di';
+import {
+  SiJest,
+  SiRedux,
+  SiTestinglibrary,
+  SiTypescript,
+  SiPython,
+  SiTailwindcss,
+  SiGraphql,
+  SiStyledcomponents
+} from 'react-icons/si';
 import { GrMysql } from 'react-icons/gr';
 import { BiConversation } from 'react-icons/bi';
 import { RiTeamFill } from 'react-icons/ri'
 import { GiBrain } from 'react-icons/gi';
 
 const hardSkills = [
-  [AiOutlineGithub, 'GitHub'],
+  [IoLogoJavascript, 'Javascript'],
+  [SiTypescript, 'Typescript'],
+  [SiPython, 'Python'],
   [AiFillHtml5, 'HTML 5'],
   [DiCss3, 'CSS 3'],
-  [IoLogoJavascript, 'Javascript'],
+  [DiSass, 'Sass'],
+  [SiTailwindcss, 'Tailwindcss'],
+  [SiStyledcomponents, 'Styled/components'],
   [SiJest, 'Jest'],
   [FaReact, 'React'],
   [SiRedux, 'Redux'],
   [SiTestinglibrary, 'RTL'],
-  [FaDocker, 'Docker'],
-  [GrMysql, 'MySQL'],
   [FaNodeJs, 'Node.js'],
+  [GrMysql, 'MySQL'],
+  [DiMongodb, 'MongoDB'],
+  [SiGraphql, 'GraphQL'],
+  [FaDocker, 'Docker'],
+  [AiOutlineGithub, 'GitHub'],
 ];
 
 const softSkills = [
