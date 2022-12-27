2067b4a014a19df03dc3660b2f361ce0590ac0b6
Braien Machado de Paula <braienmp@outlook.com>
2022-12-27 14:49:59 -0300

    style: fix long lines - set long styles in css files - break strings in multiple lines

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import { AiOutlineGithub, AiFillLinkedin } from 'react-icons/ai';
-// const profilePic = require('../assets/profile-pic.jpeg');
 
-// add typing animation do "Desenvolvedor Front-end"
+import './Home.css';
+
 export default function Home() {
   return (
-    <main id="home" className='relative min-h-screen py-12 bg-zinc-800 text-white flex flex-col md:flex-row items-center justify-evenly'>
-      <h1 className='flex flex-col text-center md:items-start items-center m-4 text-3xl md:text-4xl cursor-default'>Olá! Meu nome é
-        <span className='text-6xl md:text-6xl mt-4'>Braien Machado.</span>
-        <span className='mt-4'>Desenvolvedor Full Stack</span>
+    <section id="home">
+      <h1 className="home-heading">
+        Olá! Meu nome é
+        <span className="text-6xl md:text-6xl mt-4">Braien Machado.</span>
+        <span className="mt-4">Desenvolvedor Full Stack</span>
       </h1>
-      <img className='hidden md:block rounded-full w-36 shadow-md shadow-black mt-4 md:w-1/3' src="/src/assets/profile-pic.jpeg" alt="Braien Machado" />
-      <div className='absolute bottom-7 flex w-32 justify-between'>
-        <a className='hover:text-green-300 transition-colors' href='https://github.com/braien-machado' target='_blank' rel='noreferrer'>
-            <AiOutlineGithub size={50} />
+      <img
+        className="hidden md:block rounded-full w-36 shadow-md shadow-black mt-4 md:w-1/3"
+        src="/src/assets/profile-pic.jpeg"
+        alt="Braien Machado"
+      />
+      <div className="absolute bottom-7 flex w-32 justify-between">
+        <a
+          className="hover:text-green-300 transition-colors"
+          href="https://github.com/braien-machado"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiOutlineGithub size={50} />
         </a>
-        <a className='hover:text-green-300 transition-colors' href='https://linkedin.com/in/braien-machado' target='_blank' rel='noreferrer'>
+        <a
+          className="hover:text-green-300 transition-colors"
+          href="https://linkedin.com/in/braien-machado"
+          target="_blank"
+          rel="noreferrer"
+        >
           <AiFillLinkedin size={50} />
         </a>
       </div>
-    </main>
+    </section>
   );
 }
--- src/components/MenuLinks.jsx
+++ src/components/MenuLinks.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
+import './MenuLinks.css';
 
 export default function MenuLinks({ toggleMenu }) {
-
   const menuLinks = [
-    { link: 'Início', id: '#home'},
-    { link: 'Habilidades', id: '#skills'},
-    { link: 'Projetos', id: '#projects'},
-    { link: 'Sobre mim', id: '#about'},
+    { link: 'Início', id: '#home' },
+    { link: 'Habilidades', id: '#skills' },
+    { link: 'Projetos', id: '#projects' },
+    { link: 'Sobre mim', id: '#about' },
   ];
 
-  const generateMenuLink = ({ link, id }) => {
-    return (
-      <a className="sm:mx-4" href={id} key={ link } to={ id } onClick={ () => {
-        toggleMenu()
-      }}>
-        <li className='w-fit text-center hover:text-green-300 hover:cursor-pointer text-white transition-colors'>
-          { link }
-        </li>
-      </a>
-    );
-  };
+  const generateMenuLink = ({ link, id }) => (
+    <a
+      className="sm:mx-4"
+      href={id}
+      key={link}
+      to={id}
+      onClick={() => {
+        toggleMenu();
+      }}
+    >
+      <li className="menu-link">
+        {link}
+      </li>
+    </a>
+  );
 
   return (
-    <ul className='flex flex-col w-full sm:flex-row items-center text-lg mt-4 sm:mt-0 gap-4 sm:gap-0'>
+    <ul className="links-container">
       {menuLinks.map((link) => generateMenuLink(link))}
     </ul>
-  )
+  );
 }
