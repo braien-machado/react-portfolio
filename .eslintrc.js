70dea3ac7250b6c594e74917094d27133e9601d8
Braien Machado de Paula <braienmp@outlook.com>
2022-12-27 13:35:05 -0300

    build: change lint scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: ['plugin:react/recommended', 'airbnb'],
   parser: '@typescript-eslint/parser',
@@ -14,11 +15,19 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
+    'import/extensions': [
+      'error',
+      {
+        tsx: 'never',
+      },
+    ],
+
     'react/jsx-filename-extension': [
       1,
       { extensions: ['.tsx', '.jsx', '.ts'] },
     ],
     'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'off',
     'react/require-default-props': 'off',
     'react/prop-types': 'off',
     'max-len': ['error', { code: 100 }],
