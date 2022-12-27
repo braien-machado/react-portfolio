7f92be4ceeeadc9543c57da2c5e0508fadd178fc
Braien Machado de Paula <braienmp@outlook.com>
2022-12-27 11:26:56 -0300

    build: config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".jsx", ".ts"] }],
+        "react/react-in-jsx-scope": "off",
+        "react/require-default-props": "off",
+        "react/prop-types": "off"
+    }
+}
