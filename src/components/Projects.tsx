4960776366e30a45f2f3d71fcd65345749085017
Braien Machado de Paula <braienmp@outlook.com>
2022-12-30 01:16:41 -0300

    refactor: remove unneeded code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,13 +3,16 @@ import projectsInfo from '../projectsInfo';
 
 export default function Projects() {
   return (
-    <main id="projects" className="min-h-screen py-12  bg-zinc-800 text-white">
+    <section
+      id="projects"
+      className="min-h-screen py-12  bg-zinc-800 text-white"
+    >
       <h1 className="text-4xl text-center my-4">Projetos</h1>
       <div className="flex justify-evenly flex-wrap">
         {projectsInfo.map((project) => (
           <ProjectCard project={project} key={project.name} />
         ))}
       </div>
-    </main>
+    </section>
   );
 }
--- src/components/SkillsLibrary.tsx
+++ src/components/SkillsLibrary.tsx
@@ -5,35 +5,20 @@ interface SkillsLibraryProps {
   title: string;
 }
 
-const handleWords = (string: string) => {
-  const arrayString = string.split('/');
-  if (arrayString.length > 1) {
-    return arrayString.map((word) => (
-      <h2 key={word} className="text-2xl">
-        {word}
-      </h2>
-    ));
-  }
-
-  return <h2 className="text-2xl">{string}</h2>;
-};
-
-const generateSkill = ([Icon, name]: SkillType) => (
-  <div
-    key={name}
-    className="m-2 flex flex-col justify-center items-center w-40 h-40"
-  >
-    <Icon size={100} />
-    {handleWords(name)}
-  </div>
-);
-
 export default function SkillsLibrary({ skills, title }: SkillsLibraryProps) {
   return (
     <div className="">
       <h1 className="text-center text-4xl my-4">{title}</h1>
       <div className="flex flex-wrap justify-center">
-        {skills.map(generateSkill)}
+        {skills.map(([Icon, name]: SkillType) => (
+          <div
+            key={name}
+            className="m-2 flex flex-col justify-start items-center w-40 h-40"
+          >
+            <Icon size={100} />
+            <h2 className="text-2xl text-center">{name}</h2>
+          </div>
+        ))}
       </div>
     </div>
   );
